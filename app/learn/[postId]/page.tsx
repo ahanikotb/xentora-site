387a187ae99b2f840ba97446d990ca1d73b0d13d
Ahmed Kotb <ahanikotb@gmail.com>
2023-06-29 15:44:19 +0300

    gs

--- app/learn/[postId]/page.tsx
+++ app/learn/[postId]/page.tsx
@@ -3,6 +3,7 @@ import fs from "fs";
 import ReactMarkdown from "react-markdown";
 import Nav from "@/components/Nav";
 import rehypeRaw from "rehype-raw";
+import { PostData } from "./PostData";
 
 type Props = {
   params: {
@@ -24,17 +25,6 @@ function Page({ params: { postId } }: Props) {
 }
 
 export default Page;
-
-("use client");
-export function PostData({ post }: any) {
-  return (
-    <ReactMarkdown
-      rehypePlugins={[rehypeRaw]}
-      children={post}
-      className=" text-2xl prose-invert  prose xl:prose-2xl xl:text-2xl"
-    ></ReactMarkdown>
-  );
-}
 
 function getPostBySlug(slug: string) {
   return fs.readFileSync(`./public/posts/${slug}.mdX`, "utf-8");
