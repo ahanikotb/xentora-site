1b29e0c9de78366f7f09852fa4fe8e3bca48ddeb
Ahmed Kotb <ahanikotb@gmail.com>
2023-07-12 08:16:42 +0300

    u

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ const RESTRICTED_COUNTRIES = ["IN", "EG"];
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const res = NextResponse.next();
   const country = request.geo?.country ?? "";
-  if (RESTRICTED_COUNTRIES.includes(country)) {
-    return NextResponse.rewrite(new URL("/restricted", request.url));
-  }
+  // if (RESTRICTED_COUNTRIES.includes(country)) {
+  //   return NextResponse.rewrite(new URL("/restricted", request.url));
+  // }
   return res;
 }
