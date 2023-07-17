30bab4b53f833d7fa707a48d0c5286aab572ae6f
Ahmed Kotb <ahanikotb@gmail.com>
2023-07-17 22:35:54 +0300

    t

--- app/go/inbound-leads/page.tsx
+++ app/go/inbound-leads/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-import React, { useLayoutEffect } from "react";
-import { useRouter } from "next/navigation";
-import { Loader2 } from "lucide-react";
-function page() {
-  const router = useRouter();
-  useLayoutEffect(() => {
-    router.push("/go/inbound-leads/1");
-  }, []);
-  return (
-    <div className="w-screeb h-screen bg-white flex items-center justify-center">
-      <Loader2 className="animate-spin w-10 h-10"></Loader2>
-    </div>
-  );
-}
+// "use client";
+// import React, { useLayoutEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { Loader2 } from "lucide-react";
+// function page() {
+//   const router = useRouter();
+//   useLayoutEffect(() => {
+//     router.push("/go/inbound-leads/1");
+//   }, []);
+//   return (
+//     <div className="w-screeb h-screen bg-white flex items-center justify-center">
+//       <Loader2 className="animate-spin w-10 h-10"></Loader2>
+//     </div>
+//   );
+// }
 
-export default page;
+// export default page;
